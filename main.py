bca1f313845dbc9ba9641e228fb0d3208a8e7905
antipups <nick.kurkurin@protonmail.com>
2020-04-12 19:01:26 +0300

    Сделан парсер.

--- main.py
+++ main.py
@@ -1,136 +1,14 @@
-# from kivy.app import App
-# from kivy.uix.button import Button
-#
-#
-# class MyApp(App):
-#     def build(self):
-#         return Button()
-#
-#
-# if __name__ == '__main__':
-#     MyApp().run()
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
-# import pymysql
-# connect = pymysql.Connect(user='root', password='', db='test', host='localhost')
-# cursor = connect.cursor()
-# # cursor.execute('SELECT * FROM main_lot')
-# cursor.execute('INSERT INTO sex (first_col, sec_col) VALUES (20, 20), (30, 30)')
-# connect.commit()
-# connect.close()
+
+class MyApp(App):
+    def build(self):
+        sm = ScreenManager()
+        return sm
 
 
-# class execute:
-#     def __init__(self):
-#         self.connect = pymysql.Connect(user='root', password='', db='school_work', host='localhost')
-#
-#     def __enter__(self):
-#         return self.connect.cursor()
-#
-#     def __exit__(self, exc_type, exc_val, exc_tb):
-#         print('Конеект закрыт')
-#         self.connect.close()
-#
-#
-# with execute() as cursor:
-#     cursor.execute('SELECT * FROM main_lot')
-#     print(cursor.fetchall())
-#     # cursor.execute('INSERT INTO main_country (sex, keks) VALUES ("")');
-#     raise Exception
-
-
-# for i in range(0, 230, 50):
-#     print(i)
-# else:
-#     print(i)
-
-
-# try:
-#     raise Exception
-# except:
-#     try:
-#         print(1/0)
-#     except ZeroDivisionError:
-#         print('lox')
-
-
-# import pymysql.cursors
-# import config
-
-# paramstyle = "%s"
-#
-# def connect():
-#     """
-#      Подключение к базе данных
-#     """
-#     return pymysql.connect(
-#         config.db_host,
-#         config.db_user,
-#         config.db_password,
-#         config.db_database,
-#         use_unicode=True,
-#         charset=config.db_charset,
-#         cursorclass=pymysql.cursors.DictCursor)
-#
-#
-# def execute(sql, *args, commit=False):
-#     """
-#      Формат запроса:
-#      execute('<Запрос>', <передаваемые параметры>, <commit=True>)
-#     """
-#     db = connect()
-#     cur = db.cursor()
-#     try:
-#         cur.execute(sql % {"p": paramstyle}, args)
-#     except pymysql.err.InternalError as e:  # обработайся тут сука
-#         if sql.find('texts') == -1:
-#             print('Cannot execute mysql request: ' + str(e))
-#         return 'Error'
-#     except ValueError:  # если вдруг ошибка с символами
-#         return 'unsupported format character'
-#     if commit:
-#         db.commit()
-#         db.close()
-#         return True
-#     else:
-#         ans = cur.fetchall()
-#         db.close()
-#         return ans
-
-
-import xlrd, xlwt
-
-
-import random
-all_numbers = tuple(str(random.randint(0, 9)) for x in range(150))
-amount_of_numbers = {'0': 0,
-                     '1': 0,
-                     '2': 0,
-                     '3': 0,
-                     '4': 0,
-                     '5': 0,
-                     '6': 0,
-                     '7': 0,
-                     '8': 0,
-                     '9': 0}
-
-print(all_numbers)
-for i in all_numbers:
-    print(i)
-    for j in i:
-        amount_of_numbers[j] += 1
-
-print(amount_of_numbers, sum(x for x in amount_of_numbers.values()))
-wb = xlwt.Workbook()
-ws = wb.add_sheet('Test')
-for i in range(150):
-    ws.write(i, 0, int(all_numbers[i]))
-i = 3
-
-for key, value in amount_of_numbers.items():
-    ws.write(0, i, int(key))
-    ws.write(1, i, value)
-    ws.write(2, i, f'=(-100*0,1)/(100*0,1)')
-    i += 1
-
-wb.save('sex.xls')
+if __name__ == '__main__':
+    MyApp().run()
---
+++ parser.py
@@ -0,0 +1,48 @@
+import requests
+import re
+
+
+def parse():
+    # задаем начальные данные
+    data = {
+        'page': '1',
+        'mode': 'date',
+        'super': 'false',
+        'from': '10.10.2015',
+        'to': '12.04.2020',
+    }
+    request = requests.post('https://www.stoloto.ru/draw-results/keno/load', data=data)
+
+    old_tirags = list()
+    with open('test.txt', 'r', encoding='utf-8') as f:
+        old_tirags = tuple(f.readlines())
+
+    last_tirag = str()
+    if len(old_tirags) > 1:
+        last_tirag = old_tirags[-1]
+
+    list_of_new_tirags = list()
+
+    with open('test.txt', 'w', encoding='utf-8') as f:
+        while request.status_code == 200:
+            request = request.text
+            for one_tirag in re.findall(r"<div class=\\\"draw_date\\\"[^⚲]*</span>", request):
+                date = 'Дата тиража - ' + re.search(r'\d\d\.\d\d\.\d{4} \d\d:\d\d:\d\d', one_tirag).group() + '; '
+                if last_tirag.startswith(date):
+                    f.write(''.join(old_tirags + tuple(list_of_new_tirags[::-1])))
+                    print('Данные добавлены')
+                    return
+                else:
+                    one_tirag = one_tirag[one_tirag.find('<div class=\\"container cleared\\">'):]
+                    bals = re.findall(r'\d\d?\d?', one_tirag)
+                    list_of_new_tirags.append(date + ', '.join(bals) + '\n')
+            data['page'] = str(int(data.get('page')) + 1)
+            request = requests.post('https://www.stoloto.ru/draw-results/keno/load', data=data)
+            print("Страница № " + data.get('page'))
+        else:
+            f.write('\n'.join(tuple(list_of_new_tirags[::-1])))
+            print("Все данные считаны")
+
+
+if __name__ == '__main__':
+    parse()
